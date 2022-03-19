13d78d06c5c7489f39fca0794a7c07e896003f5d
ydlong <donlongca@gmail.com>
2022-03-19 11:45:27 -0400

    New

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'faceweb-app';
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
 }
